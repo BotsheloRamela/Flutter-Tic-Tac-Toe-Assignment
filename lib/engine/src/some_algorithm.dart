e96ab12a49b2224a1b7c8ba83b6af8c4eadcac2f
Botshelo Ramela <ramelabotshelo12@gmail.com>
2024-10-08 22:07:45 +0200

    FEAT: New _checkLine function to check a winning line

--- lib/engine/src/some_algorithm.dart
+++ lib/engine/src/some_algorithm.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:munch_test/engine/engine.dart';
 import 'package:munch_test/engine/src/constants.dart';
 
@@ -9,8 +11,12 @@ class SomeAlgorithm extends IAlgorithm {
     throw UnimplementedError();
   }
 
-  @override
-  List<MarkedPoint> processMove(List<MarkedPoint> givenPositions) {
-    throw UnimplementedError();
+  /// Checks if three points form a winning line
+  bool _checkLine(List<MarkedPoint> givenPositions, Point a, Point b, Point c) {
+    var pointA = givenPositions.firstWhere((p) => p.position == a, orElse: () => MarkedPoint(value: -1, position: a));
+    var pointB = givenPositions.firstWhere((p) => p.position == b, orElse: () => MarkedPoint(value: -1, position: b));
+    var pointC = givenPositions.firstWhere((p) => p.position == c, orElse: () => MarkedPoint(value: -1, position: c));
+
+    return pointA.value != -1 && pointA.value == pointB.value && pointB.value == pointC.value;
   }
 }
